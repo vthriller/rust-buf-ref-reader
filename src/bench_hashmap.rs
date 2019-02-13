aa8742f2f9d8f0880c8cbaefc00fcb13f3ce97e9
vthriller <unixway.drive+git@gmail.com>
2019-02-13 23:52:47 +0300

    hold on, this isn't what this baseline benchmark should measure
    test bench_hashmap::baseline_2           ... bench:   4,122,292 ns/iter (+/- 115,594)
    test bench_hashmap::baseline_3           ... bench:   5,163,578 ns/iter (+/- 34,769)
    test bench_hashmap::baseline_4           ... bench:   8,569,902 ns/iter (+/- 179,624)

--- src/bench_hashmap.rs
+++ src/bench_hashmap.rs
@@ -76,42 +76,31 @@ fn std_read_until(b: &mut Bencher, buf: usize, n: usize, cap: usize) {
 #[bench] fn std_read_until_64k_3(b: &mut Bencher) { std_read_until(b, 65536, 3, 6500) }
 #[bench] fn std_read_until_64k_4(b: &mut Bencher) { std_read_until(b, 65536, 4, 28000) }
 
+/*
+this benchmark is solely about measuring code
+that populates HashMap with occasional copies of references to slices of WORDS,
+hence collection of such slices outside the bench loop
+*/
 fn baseline(b: &mut Bencher, n: usize, cap: usize) {
+	let words: Vec<usize> = memchr_iter(b'\n', WORDS)
+		.collect(); // can't clone Memchr iterator itself, hence this
+	let starts = vec![0].into_iter()
+		.chain(words.clone().into_iter().map(|n| n+1)); // one past delimiter
+	let ends = words.into_iter()
+		.chain(vec![WORDS.len()].into_iter());
+	let lines: Vec<_> = starts.zip(ends)
+		.map(|(start, end)| &WORDS[start..end])
+		.collect();
+
 	b.iter(|| {
 		let mut map = map(cap);
-		/*
-		I tried .peekable(), but .peek() inside a loop ends up making two mutable refs (E0499),
-		so instead of hacking own loop with .next()/.peek() I just wrote C-style thing with mutable vars.
-
-		Another option is to
-
-		let mut words: Vec<usize> = memchr_iter(b'\n', WORDS)
-			.collect(); // can't clone Memchr iterator itself, hence this
-		let mut starts = vec![0].into_iter()
-			.chain(words.clone().into_iter().map(|n| n+1));
-		let mut ends = words.into_iter()
-			.chain(vec![WORDS.len()].into_iter());
-
-		to get (start, end) pairs to use directly in a slice;
-		this, however, results in a massive performance hit (20% to 30%).
-		*/
-		let mut words = memchr_iter(b'\n', WORDS);
-		let mut start = 0;
-		loop {
-			let end = match words.next() {
-				Some(n) => n,
-				None => break,
-			};
-			let line = &WORDS[start..end];
-
+		for &line in lines.iter() {
 			// .entry() does not accept Borrow<K>, hence this
 			let p = prefix(&line, n);
 			match map.get_mut(p) {
 				Some(v) => { *v += 1; },
 				None => { map.insert(p.to_vec(), 1); },
 			}
-
-			start = end + 1;
 		}
 	})
 }
