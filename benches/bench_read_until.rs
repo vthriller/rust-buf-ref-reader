06a2455020e859c2bb66c177d46661081517a859
vthriller <unixway.drive+git@gmail.com>
2019-02-16 06:34:40 +0300

    + std_fillbuf_4k: I decided to also copy here benchmark from another experiment

--- benches/bench_read_until.rs
+++ benches/bench_read_until.rs
@@ -2,6 +2,7 @@ use bencher::{Bencher, benchmark_group, benchmark_main};
 
 use buf_ref_reader::*;
 use std::io::{BufRead, BufReader};
+use memchr::memchr;
 
 static WORDS: &'static [u8] = include_bytes!("/usr/share/dict/words");
 
@@ -29,6 +30,72 @@ fn std_read_until(b: &mut Bencher, cap: usize) {
 fn std_read_until_16(b: &mut Bencher) { std_read_until(b, 16) }
 fn std_read_until_64(b: &mut Bencher) { std_read_until(b, 64) }
 fn std_read_until_4k(b: &mut Bencher) { std_read_until(b, 4096) }
+
+/*
+This one is like BufRefReader that's made of parts of BufReader,
+except when requested data continues past buffer boundary:
+
+| buffer |
+      | data |
+
+in which case leftovers are moved into temporary buffer,
+and then the main buffer is refilled entirely
+to complete that temporary buffer first.
+
+Temporary buffer is discarded upon next read,
+and regular referencing of parts of the main buffer is resumed.
+*/
+fn std_fillbuf_4k(b: &mut Bencher) {
+	b.iter(|| {
+		let mut r = BufReader::with_capacity(4096, &WORDS[..]);
+
+		let mut head: Option<Vec<u8>> = None;
+
+		loop {
+			let buf = r.fill_buf().unwrap();
+			if buf.len() == 0 {
+				// EOF
+
+				if let Some(head) = &mut head {
+					// F(head.as_slice());
+				}
+
+				break;
+			}
+
+			match memchr(b'\n', buf) {
+				Some(len) => {
+					let tail = &buf[..len];
+					let s = if let Some(head) = &mut head {
+						head.extend_from_slice(tail);
+						head.as_slice()
+					} else {
+						tail
+					};
+
+					// F(s);
+
+					head = None;
+					// and only now, after we've used `tail`, we consume data referenced via said `tail`
+					r.consume(len+1);
+				},
+				None => {
+					// Either line doesn't fit into the buffer (or, at the very least, tail of the buffer),
+					// or the last line does not contain any b'\n'.
+					// Resort to copying for this (hopefully) very rare case.
+					if let Some(head) = &mut head {
+						// again?!
+						head.extend_from_slice(buf);
+					} else {
+						head = Some(buf.to_vec());
+					}
+					let len = buf.len();
+					r.consume(len);
+				},
+			}
+		}
+	})
+}
 
 // like read_until_words_long test, split by the most rare character in WORDS:
 
@@ -58,6 +125,7 @@ benchmark_group!(benches,
 	std_read_until_16,
 	std_read_until_64,
 	std_read_until_4k,
+	std_fillbuf_4k,
 	bufref_read_until_long,
 	std_read_until_long,
 );
